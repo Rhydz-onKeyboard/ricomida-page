f693342eec84bf99e5fb1e00fbe23e38043bd58a
aldiaza <alr.diaza@gmail.com>
2021-07-01 14:42:45 -0400

    inicio con jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,27 @@ $(function(){
         if (width>=768) {
             $('#first-divider').hide();
             $('.slider').show();
+            $('#title').css("text-align", "left");
+            $('#btns').css("justify-content", "start");
+            $('header').css("padding-top", "2rem");
         } else {
             $('#first-divider').show();
             $('.slider').hide();
+            $('#title').css("text-align", "center");
+            $('#btns').css("justify-content", "center");
+            $('header').css("padding-top", "1rem");
         }
         //console.log(width);
-    })
+    });
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+    
+    $(document).ready(function(){
+        $('.carousel').carousel({
+            interval: 2000
+        });
+    });
 });
