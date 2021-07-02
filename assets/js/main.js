bb274414fdb0db65d356555a61ef7cd755d04530
aldiaza <alr.diaza@gmail.com>
2021-07-01 23:04:39 -0400

    integrando jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,31 @@
 $(function(){
     // métodos de jQuery...
 
-    $(window).resize( function(){
-        const { width } = window.screen;
+    const { width } = window.screen;
         if (width>=768) {
             $('#first-divider').hide();
-            $('.carousel-rico').show();
             $('#title').css("text-align", "left");
             $('#title').css("padding-right", "9rem");
             $('#btns').css("justify-content", "start");
             $('header').css("padding-top", "2rem");
         } else {
             $('#first-divider').show();
-            $('.carousel-rico').hide();
+            $('#title').css("text-align", "center");
+            $('#title').css("padding-right", "0");
+            $('#btns').css("justify-content", "center");
+            $('header').css("padding-top", "1rem");
+        }
+
+    $(window).resize( function(){
+        const { width } = window.screen;
+        if (width>=768) {
+            $('#first-divider').hide();
+            $('#title').css("text-align", "left");
+            $('#title').css("padding-right", "9rem");
+            $('#btns').css("justify-content", "start");
+            $('header').css("padding-top", "2rem");
+        } else {
+            $('#first-divider').show();
             $('#title').css("text-align", "center");
             $('#title').css("padding-right", "0");
             $('#btns').css("justify-content", "center");
@@ -21,13 +34,26 @@ $(function(){
         //console.log(width);
     });
 
+
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
+
+    $('#enviarCorreo').click(function (){
+        alert("El correo fue enviado correctamente...")
+    })
     
-    $(document).ready(function(){
-        $('.carousel').carousel({
-            interval: 2000
-        });
+
+    $('.carousel').carousel({
+        interval: 3000
     });
+
+    $("h5.step-section").on('dblclick', function(){
+        ($(this).css("color", "red"));
+    });
+
+    $('.card').click(function(){
+        $('.card-text').toggle()
+    })
+
 });
